3887a653e616c19b4f82b2ff3762d224d4b5987f
Eeshansh <111873887+Eeshansh@users.noreply.github.com>
2025-07-09 15:04:12 +0000

    summary of datatypes

---
+++ basic/datatypes-summary.js
@@ -0,0 +1,31 @@
+// primitive datatypes
+//  7 types : string, Number, Boolean, undefined, null, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.5
+
+// const isLoggedIn = false
+// const outsideTemp = null
+
+// let userEmail;
+// const ID = Symbol('123')
+// const anotherID = Symbol('123')
+
+// console.log(ID === anotherID);
+
+// const bigNum = 5342318n
+
+// console.log(typeof bigNum);
+
+
+
+// Reference (Non-primitive)
+// Array, Objects, Functions
+
+const heros = ["Hanuman","Ironman", "Captain America", "Hulk", "Thor"]
+
+let obj1 = {
+    name: "Eeshansh",
+    age: 23
+}
+
