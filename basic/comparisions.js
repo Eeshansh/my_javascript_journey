b17110e36da6e8f45794ebc4ee851c94531d313c
Eeshansh <111873887+Eeshansh@users.noreply.github.com>
2025-07-09 14:06:39 +0000

    comparisions concept

---
+++ basic/comparisions.js
@@ -0,0 +1,13 @@
+// console.log("2">1);
+
+// console.log(null>0);     /each one of them is false because null is not greater than 0
+// console.log(null<0);     /each one of them is false because null is not less than 0
+// console.log(null == 0)     /each one of them is false because null is not equal to 0
+
+// console.log(null>=0); // this is true because comparision operator converts/treats the null as a whole number greater than o
+
+// console.log(undefined > 0); / undefined in any case is 'false'
+
+    // '===' : this is called strict check, checks for the datatypes also in comparisions.
+    /*Example: */ console.log("2" === 2);
+    
---
+++ basic/conversion_operation.js
@@ -0,0 +1,37 @@
+// let score = false
+
+// console.log(typeof score);
+
+// let value = Number(score)
+// console.log(typeof value);
+// // console.log(value);
+
+// let logged = "eb"
+// let bool_looged = Boolean(logged)
+
+// console.log(typeof logged);
+// console.log(logged);
+
+// console.log(typeof bool_looged);
+// console.log(bool_looged);
+
+/* 1 => number => true, 0 => false
+  "1","eb" => true
+  "" => false */
+
+/**************************************************Opertions*****************************/
+
+let value = 3
+
+let negvalue = -value
+
+// console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " EB"
+
+let str3 = str1 + str2
+
+// console.log(+""); 
+
+
