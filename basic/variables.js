f035cfd94b3c204e6ed958e949dc52b1ac0f16a7
Eeshansh <111873887+Eeshansh@users.noreply.github.com>
2025-07-09 06:09:59 +0000

    datatypes

---
+++ basic/variables.js
@@ -0,0 +1,9 @@
+const account_id = 144553  //cannot change this value for account_id variable
+let account_email = "Eeshansh@google.com"
+var account_password = "12345"
+account_city = "Jaipur"
+
+
+account_id = 2
+
+console.log(account_id);
