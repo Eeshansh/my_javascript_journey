9578eba6f16d0a7ff7083b073e7e58df4283a60a
Eeshansh <111873887+Eeshansh@users.noreply.github.com>
2025-07-09 06:10:55 +0000

    datatypes

---
+++ basic/data_types.js
@@ -0,0 +1,17 @@
+"use strict" //treats all JS code as newer version.
+// alert(3+3) // we are using node.js so alert won't work properly as it isn't browser.
+
+/* Data types in Javascript
+    number => 2 power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined => value not defined or given
+    symbol => unique
+    
+    
+    object*/
+
+    console.log(typeof undefined);
+    
