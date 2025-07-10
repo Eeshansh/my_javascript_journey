ba607a2d0ac746a811c24490aa3107082597baeb
Eeshansh <111873887+Eeshansh@users.noreply.github.com>
2025-07-10 10:30:17 +0000

    array discussion

---
+++ basic/array.js
@@ -0,0 +1,49 @@
+
+const heroes = ['Hanuman','Ironman','Thor','Hulk','Captain America']
+
+
+// console.log(arr[5]);
+// console.log(heroes[0]);
+
+/*******Methods of Array*********/
+// // PUSH
+// arr.push(500);
+// arr.pop()
+
+
+// arr.unshift(9)
+// console.log(arr);
+// arr.shift()
+// console.log(arr.includes(234));
+// console.log(arr.indexOf(234));
+
+// const new_arr = arr.join()
+
+// console.log(arr);
+// console.log(typeof arr);
+
+// console.log(new_arr);
+// console.log(typeof new_arr);
+
+
+// SLICE and SPLICE
+const arr = [1,2,3,4,5,6]
+console.log('A',arr);
+
+const myn1 = arr.slice(1,3)
+console.log(myn1);
+
+console.log('B',arr);
+
+const myn2 = arr.splice(1,3)
+console.log(myn2);
+
+console.log(`this is the original array which is manipulated due to Splice operation:  ${arr}`);
+
+
+
+
+
+
+
+
