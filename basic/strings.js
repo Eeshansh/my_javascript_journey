c27696189df10b2430d0126bdf888b55e4f5c9e6
Eeshansh <111873887+Eeshansh@users.noreply.github.com>
2025-07-10 06:44:40 +0000

    strings learnt...

---
+++ basic/strings.js
@@ -0,0 +1,29 @@
+const name = "Eeshansh-18"
+const  age = 23
+const comp = "years old"
+/* normal/basic way of declaring the string in JS*/
+
+// const name1 = new String("Eeshansh")
+/*Another way of declaring the string in JS*/
+
+// console.log(name,"is",age,comp); 
+/* this is an older version of concatention of string. Using comma or '+' isn't recommended in the industry standards.*/
+
+// console.log(`Hello my name is ${name}. I'm ${age} ${comp}`); 
+/*this is the real industry standards way of doing concatenation in the strings.*/
+
+console.log(name.toUpperCase());
+// console.log(name.charAt(4));
+// console.log(name.indexOf('a'));
+// console.log(name.substring(0,8));
+// console.log(name.slice(-8,-5));
+// console.log(name.trim());
+// console.log(name.replace('a', 'K'));
+// console.log(name.includes('58'));  //this checks for a particular characters present in the string.
+// console.log(name.split('-'));     //convert strings to arrays.
+ 
+
+
+
+
+
