eadcded5ac39b08e2db43cdcef42066d25b2a3d0
Eeshansh <111873887+Eeshansh@users.noreply.github.com>
2025-07-10 08:07:38 +0000

    numbers and math library

---
+++ basic/numbers_and_math.js
@@ -0,0 +1,20 @@
+// const num = 369.26543
+// console.log(num.toPrecision(4));
+
+// const expo = 10000000000
+
+// console.log(expo.toLocaleString('en-IN'));
+
+/**********************************************maths***********************************************************/
+
+// // console.log(Math.floor(4.9));
+// console.log(Math.min(3,4,5,7,12,));
+
+// console.log(Math.max(3,4,5,7,12,));
+
+console.log(Math.random());
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
